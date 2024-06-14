ec3df28dcd03bcbbeb274b02bc25b9d787edc812
Joao <engjobe@gmail.com>
2024-06-13 22:11:26 -0300

    desafio de projeto

---
+++ aulas/index.js
@@ -0,0 +1,27 @@
+// Variáveis para armazenar o nome e a quantidade de experiência (XP) do herói
+var nomeDoHeroi = "Pikachu";
+var xpDoHeroi = 7000;
+
+// Estrutura de decisão para determinar o nível do herói com base na XP
+var nivelDoHeroi;
+
+if (xpDoHeroi < 1000) {
+    nivelDoHeroi = "Ferro";
+} else if (xpDoHeroi >= 1001 && xpDoHeroi <= 2000) {
+    nivelDoHeroi = "Bronze";
+} else if (xpDoHeroi >= 2001 && xpDoHeroi <= 5000) {
+    nivelDoHeroi = "Prata";
+} else if (xpDoHeroi >= 5001 && xpDoHeroi <= 7000) {
+    nivelDoHeroi = "Ouro";
+} else if (xpDoHeroi >= 7001 && xpDoHeroi <= 8000) {
+    nivelDoHeroi = "Platina";
+} else if (xpDoHeroi >= 8001 && xpDoHeroi <= 9000) {
+    nivelDoHeroi = "Ascendente";
+} else if (xpDoHeroi >= 9001 && xpDoHeroi <= 10000) {
+    nivelDoHeroi = "Imortal";
+} else {
+    nivelDoHeroi = "Radiante";
+}
+
+// Exibindo a mensagem com o nome e o nível do herói
+console.log("O Herói de nome " + nomeDoHeroi + " está no nível de " + nivelDoHeroi);
